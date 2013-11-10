7902e3264b481ed1aef09a55d589fa7b5e8c8552
Adam Wright <adam@hipikat.org>
2013-11-10 15:41:21 +0800

    Add debug_pdb utility

--- revkom/utils/__init__.py
+++ revkom/utils/__init__.py
@@ -1,2 +1,22 @@
 
+import warnings
+from django.conf import settings
 from .mixins import GetSettingsMixin
+
+
+def _debug_pdb():
+    """
+    Return a pdb module if settings.DEBUG is True. Otherwise, issue
+    an ImportWarning and return a stub object with a set_trace method
+    that does nothing.
+    """
+    class StubPdb(object):
+        def set_trace():
+            pass
+
+    if settings.DEBUG:
+        import pdb
+        return pdb
+    else:
+        return StubPdb()
+debug_pdb = _debug_pdb()
